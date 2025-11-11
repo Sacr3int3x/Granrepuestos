4a1f756a81768494773976517ec261b203b7cba7
Anibal Ubieda <Aubiedag@gmail.com>
2025-11-11 17:15:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9222549075-738e2",
+  "appId": "1:740752463489:web:58f0db1d6c515425f05f80",
+  "apiKey": "AIzaSyATnXw0KQc2iJsr1-1lfDmEa3UJ8k0Ryig",
+  "authDomain": "studio-9222549075-738e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "740752463489"
+};
