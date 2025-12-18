b9c32b0d7532f2b0747e64ee4a32470df204b0a8
Anibal Ubieda <Aubiedag@gmail.com>
2025-12-18 20:13:06 +0000

    PUEDES CAMBIAR EL FOOTER, QQUE SE MUESTREN LOS BOTONES QUE ESTAN A LA DE

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="border-t bg-card">
-      <div className="container flex flex-col items-center justify-center py-4 gap-2">
-        <div className="flex items-center gap-2 text-center text-sm text-muted-foreground">
+      <div className="container flex items-center justify-between py-4">
+        <div className="flex items-center gap-2 text-sm text-muted-foreground">
           <Icons.logo className="h-8 w-10 text-primary" />
-          <p className="leading-loose">
+          <p className="leading-loose hidden sm:block">
             &copy; {new Date().getFullYear()} GranRepuestos. Todos los derechos reservados.
           </p>
         </div>
-        <div className="flex gap-4 text-xs">
+        <div className="flex gap-4 text-sm">
            <Link href="/politicas" className="text-muted-foreground hover:text-primary hover:underline">
-            Políticas de Compra
+            Políticas
           </Link>
            <Link href="/admin" className="text-muted-foreground hover:text-primary hover:underline">Admin</Link>
         </div>
